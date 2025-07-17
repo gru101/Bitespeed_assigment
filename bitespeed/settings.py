5c3e67b64fa25c79a66bdf2e3e007b009dacc32b
gru101 <grukandgal@gmail.com>
2025-07-18 01:28:09 +0530

    altered allowed hosts

--- bitespeed/settings.py
+++ bitespeed/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://bitespeed-assigment-79qu.onrender.com"]
+ALLOWED_HOSTS = ['bitespeed-assigment-79qu.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
